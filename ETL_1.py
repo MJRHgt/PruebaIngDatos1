9dc3ac9a48131383800460f0e24a45f9c7d6c70b
MJRHgt <mariojudoka@gmail.com>
2026-09-23 13:44:34 -0600

    inicio del ETL sin generar la nueva tabla

---
+++ ETL_1.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import sqlite3
+
+print("Iniciando proceso ETL (Ejercicio2)...")
+
+#Archivos de entrada
+df_clientes = pd.read_csv('clientes_actualizado.csv')
+df_transacciones = pd.read_csv('transacciones_actualizado.csv')
+
+#Transformar campo a:
+df_transacciones['FechaPago'] = pd.to_datetime(df_transacciones['FechaPago'])
+
+#Calculos
+df_agrupado = df_transacciones.groupby('ClienteID').agg(
+    MontoPagado=('MontoPagado', 'sum'),
+    PagosTotales=('ClienteID', 'count'),
+    FechaUltimoPago=('FechaPago', 'max')
+).reset_index()
+
+#Asegurar campo transformado
+df_agrupado['FechaUltimoPago'] = df_agrupado['FechaUltimoPago'].dt.strftime('%Y-%m-%d')
+
+#unir tablas
+df_leftJoin = pd.merge(df_clientes, df_agrupado, on='ClienteID', how='left')
+
+#Tranformacion si es null se va a 0
+df_leftJoin['MontoPagado'] = df_leftJoin['MontoPagado'].fillna(0)
+df_leftJoin['PagosTotales'] = df_leftJoin['PagosTotales'].fillna(0).astype(int)
+
+#Estado con regla de negocio, no contepla error
+df_leftJoin['EstadoPago'] = df_leftJoin.apply(
+    lambda fila: 'Al día' if fila['MontoPagado'] >= fila['MontoOriginal'] else 'En mora', 
+    axis=1
+)
+
+#yabla final 
+columnas_finales = [
+    'ClienteID', 'Nombre', 'MontoOriginal', 'MontoPagado', 
+    'PagosTotales', 'FechaUltimoPago', 'EstadoPago'
+]
+df_final = df_leftJoin[columnas_finales]
+
+#Revision
+print("\nVista previa de la tabla transformada:")
+print(df_final.head().to_string(index=False))
